123c3846e0c1c662c7948f1050079abdef9f184f
mohamed elassadi <mohamed.elassadi@askcharlie.com>
2018-10-21 15:12:09 +0200

    alonmzto

--- test/controllers/me_controller_test.rb
+++ test/controllers/me_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class MeControllerTest < ActionDispatch::IntegrationTest
+
+  test "the truthm " do
+     assert true
+     
+  end  
   test "the truth" do
      assert false
+     
   end
 end
