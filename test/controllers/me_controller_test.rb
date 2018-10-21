5c500fdf42d01fcd63b5a165601f12eb2ecc7267
mohamed elassadi <mohamed.elassadi@askcharlie.com>
2018-10-21 15:33:44 +0200

    fix erros

--- test/controllers/me_controller_test.rb
+++ test/controllers/me_controller_test.rb
@@ -1,12 +1,7 @@
 require 'test_helper'
 
 class MeControllerTest < ActionDispatch::IntegrationTest
-
   test "the truthm " do    
      assert true     
   end  
-  test "the truth" do
-     assert true
-     
-  end
 end
